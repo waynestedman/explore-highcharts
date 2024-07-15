06984a8a8d2df43d2f0d1184ab2bb7015e710d51
Wayne Stedman <waynestedman@users.noreply.github.com>
2024-07-15 11:41:51 -0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>React w/ Highcharts App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
